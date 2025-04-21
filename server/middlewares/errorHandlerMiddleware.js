bb10158c05fb20f44a11ae64615384f4f4062666
Satwik Kaushik <satwikkaushik05@gmail.com>
2025-04-21 05:16:13 +0000

    Bug fix in error handling middleware

--- server/middlewares/errorHandlerMiddleware.js
+++ server/middlewares/errorHandlerMiddleware.js
@@ -1,9 +1,9 @@
-function errorHandler(err, _req, res, _next) {
+function errorHandler(err, req, res, next) {
   const status = err.status || 500;
   const message = err.message || "Internal Server Error";
 
-  console.error("Error: ", message, " Status: ", status);
-  return res.status(status).json({ error: message, status: status });
+  console.error("Error:", message, "Status:", status);
+  res.status(status).json({ error: message });
 }
 
 export default errorHandler;
--- server/utils/AppError.js
+++ server/utils/AppError.js
@@ -1,6 +1,13 @@
 class AppError extends Error {
+  statusCode;
+
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
+    this.name = this.constructor.name;
+
+    Error.captureStackTrace(this, this.constructor);
   }
 }
+
+export default AppError;
